1a40a1c67e19b4edbb9a15fe903004054a24b757
pedrovitorrs <pedro442.rodrigues@gmail.com>
2024-04-06 01:08:36 -0300

    fix: altera projeto para utilizar banco de dados postgresql.

---
+++ codigo-fonte/MMSalgados.Infra.Data/Migrations/20240406035944_MyFirstMigration.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace MMSalgados.Infra.Data.Migrations
+{
+    public partial class MyFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Acesso",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    Descricao = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Situacao = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Acesso", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PerfilUsuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    Situacao = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PerfilUsuario", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MapPerfilUsuariosAcessos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PerfilUsuarioId = table.Column<int>(type: "integer", nullable: true),
+                    AcessoId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MapPerfilUsuariosAcessos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MapPerfilUsuariosAcessos_Acesso_AcessoId",
+                        column: x => x.AcessoId,
+                        principalTable: "Acesso",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_MapPerfilUsuariosAcessos_PerfilUsuario_PerfilUsuarioId",
+                        column: x => x.PerfilUsuarioId,
+                        principalTable: "PerfilUsuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Login = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    Senha = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    CpfCnpj = table.Column<string>(type: "character varying(14)", maxLength: 14, nullable: true),
+                    Nome = table.Column<string>(type: "text", nullable: true),
+                    Email = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: false),
+                    PerfilUsuarioId = table.Column<int>(type: "integer", nullable: true),
+                    Situacao = table.Column<int>(type: "integer", nullable: false),
+                    Image = table.Column<string>(type: "text", nullable: true),
+                    DataNascimento = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Usuario_PerfilUsuario_PerfilUsuarioId",
+                        column: x => x.PerfilUsuarioId,
+                        principalTable: "PerfilUsuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pedido",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Total = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    Status = table.Column<bool>(type: "boolean", nullable: false),
+                    QuantidadeItens = table.Column<int>(type: "integer", nullable: false),
+                    UsuarioId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pedido", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pedido_Usuario_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProdutoEntity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Preco = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    Descricao = table.Column<string>(type: "text", nullable: false),
+                    Image = table.Column<string>(type: "text", nullable: true),
+                    Ativo = table.Column<bool>(type: "boolean", nullable: false),
+                    Modelo = table.Column<string>(type: "text", nullable: false),
+                    Marca = table.Column<string>(type: "text", nullable: false),
+                    Processador = table.Column<string>(type: "text", nullable: true),
+                    Memoria = table.Column<string>(type: "text", nullable: true),
+                    Camera = table.Column<string>(type: "text", nullable: true),
+                    RAM = table.Column<string>(type: "text", nullable: true),
+                    UsuarioId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProdutoEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProdutoEntity_Usuario_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItemPedido",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ProdutoId = table.Column<int>(type: "integer", nullable: true),
+                    PedidoId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemPedido", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ItemPedido_Pedido_PedidoId",
+                        column: x => x.PedidoId,
+                        principalTable: "Pedido",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ItemPedido_ProdutoEntity_ProdutoId",
+                        column: x => x.ProdutoId,
+                        principalTable: "ProdutoEntity",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "PerfilUsuario",
+                columns: new[] { "Id", "Nome", "Situacao" },
+                values: new object[] { 1, "ADMIN", 1 });
+
+            migrationBuilder.InsertData(
+                table: "Usuario",
+                columns: new[] { "Id", "CpfCnpj", "DataNascimento", "Email", "Image", "Login", "Nome", "PerfilUsuarioId", "Senha", "Situacao" },
+                values: new object[] { 1, null, null, "admin@devscansados.com", "https://s.gravatar.com/avatar/aleatory?d=mm&s=45", "admin", "admin", 1, "admin@123", 1 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemPedido_PedidoId",
+                table: "ItemPedido",
+                column: "PedidoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemPedido_ProdutoId",
+                table: "ItemPedido",
+                column: "ProdutoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MapPerfilUsuariosAcessos_AcessoId",
+                table: "MapPerfilUsuariosAcessos",
+                column: "AcessoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MapPerfilUsuariosAcessos_PerfilUsuarioId",
+                table: "MapPerfilUsuariosAcessos",
+                column: "PerfilUsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pedido_UsuarioId",
+                table: "Pedido",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProdutoEntity_UsuarioId",
+                table: "ProdutoEntity",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuario_PerfilUsuarioId",
+                table: "Usuario",
+                column: "PerfilUsuarioId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ItemPedido");
+
+            migrationBuilder.DropTable(
+                name: "MapPerfilUsuariosAcessos");
+
+            migrationBuilder.DropTable(
+                name: "Pedido");
+
+            migrationBuilder.DropTable(
+                name: "ProdutoEntity");
+
+            migrationBuilder.DropTable(
+                name: "Acesso");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+
+            migrationBuilder.DropTable(
+                name: "PerfilUsuario");
+        }
+    }
+}
--- codigo-fonte/MMSalgados.Infra.IoC/DependencyInjection.cs
+++ codigo-fonte/MMSalgados.Infra.IoC/DependencyInjection.cs
@@ -29,9 +29,9 @@ namespace MMSalgados.Infra.IoC
             var enviroment = services.BuildServiceProvider().GetRequiredService<IWebHostEnvironment>();
 
             services.AddDbContext<DatabaseContext>(options =>
-                options.UseSqlServer(enviroment.IsDevelopment() ? configuration.GetConnectionString("DataContextConnection") : configuration.GetSecret("ConnectionStrings--DataContextConnection"),
+                options.UseNpgsql(enviroment.IsDevelopment() ? configuration.GetConnectionString("DataContextConnection") : configuration.GetSecret("ConnectionStrings--DataContextConnection"),
                 b => b.MigrationsAssembly(typeof(DatabaseContext).Assembly.FullName)));
-            
+
             if (!enviroment.IsDevelopment())
             {
                 services.AddSingleton(svc =>
